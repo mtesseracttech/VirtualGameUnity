4b9c8cd85c657f8efca92a838e2883876b98e75f
kaimelis <kaimelisvy24@outlook.com>
2016-04-28 23:31:14 +0200

    somechanges with tackle

--- Assets/Scripts/PlayerControlls.cs
+++ Assets/Scripts/PlayerControlls.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class PlayerControlls : MonoBehaviour {
-    public Text ammunation;
+   // public Text ammunation;
     private int ammo;
     private float deltaTime;
 
@@ -62,7 +62,7 @@ public class PlayerControlls : MonoBehaviour {
             Time.timeScale = 1f;
         }
         deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-        ammunation.text = " " + string.Format("{0:0.}", 1 / deltaTime)/*"Ammo: " + ammo*/;
+       // ammunation.text = " " + string.Format("{0:0.}", 1 / deltaTime)/*"Ammo: " + ammo*/;
         LineOfAimHandler();
         PlayerMovement();
         PlayerAndCameraRotation();
--- Assets/Scripts/Tackle.cs
+++ Assets/Scripts/Tackle.cs
@@ -3,63 +3,60 @@ using System.Collections;
 
 public class Tackle : MonoBehaviour
 {
-    public LayerMask collisionMask;
-    private float speed = 10;
-    private float journeyLength;
-    private bool disantce;
-    public bool tackleBool;
-    Vector3 startPos;
+    public LayerMask CollisionMask;
+    private float _speed = 10;
+    private float _journeyLength;
+    private bool _disantce;
+    public bool TackleBool;
+    Vector3 _startPos;
 
     void Start()
     {
-        disantce = false;
-        tackleBool = false;
+        _disantce = false;
+        TackleBool = false;
 
     }
     public void SetSpeed(float newSpeed)
     {
-        speed = newSpeed;
+        _speed = newSpeed;
     }
 
     void Update()
     {
-        startPos = transform.position;
-        journeyLength = speed;
-        CheckCollisions(journeyLength);
+        _startPos = transform.position;
+        _journeyLength = _speed;
+        CheckCollisions(_journeyLength);
     }
 
     void CheckCollisions(float moveDistance)
     {
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, journeyLength,collisionMask))
+        if (Physics.Raycast(ray, out hit, _journeyLength,CollisionMask))
         {
-            OnHitObject(hit);
-            disantce = false;
-            tackleBool = false;
-            if (Physics.Raycast(ray, out hit, 4, collisionMask))
+            _disantce = false;
+            TackleBool = false;
+            if (Physics.Raycast(ray, out hit, 4, CollisionMask))
             {
-                disantce = true;
+                _disantce = true;
                 if (Input.GetKey(KeyCode.E))
                 {
-                    disantce = false;
-                    tackleBool = true;
-                    transform.position = Vector3.Lerp(startPos, hit.collider.gameObject.transform.position, 0.03f);
-                    if (Physics.Raycast(ray, out hit, 0.6f, collisionMask))
+                    _disantce = false;
+                    TackleBool = true;
+                    transform.position = Vector3.Lerp(_startPos, hit.collider.gameObject.transform.position, 0.03f);
+                    if (Physics.Raycast(ray, out hit, 0.6f, CollisionMask))
+                    {
                         Destroy(hit.transform.gameObject);
+                        TackleBool = false;
+                        _disantce = false;
+                    } 
                 }
             }
         }
     }
-
-    void OnHitObject(RaycastHit hit)
-    {
-       
-    }
-
     void OnGUI()
     {
-        if (disantce)
+        if (_disantce)
         {
             GUI.Box(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 100, 120, 25), "Hold E");
         }
