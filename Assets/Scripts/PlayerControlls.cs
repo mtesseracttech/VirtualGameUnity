5fd878399f13d72b79e41ccd17da0bf21b4bfdde
kaimelis <kaimelisvy24@outlook.com>
2016-04-28 15:38:52 +0200

    added the scripts of majo

--- Assets/Scripts/PlayerControlls.cs
+++ Assets/Scripts/PlayerControlls.cs
@@ -1,17 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerControlls : MonoBehaviour {
-    public GameObject test;
+    public Text ammunation;
+    private int ammo;
+    private float deltaTime;
+
+    ////////////// Variables used for Line of Aim Handling /////////////
+    RaycastHit hitInfo;
+    Ray ray;
+    ////////////////////////////////////////////////////////////////////
+    ////////////// Variables used for raycasting and shooting //////////
+    public float fireDelay;
+    private float fireDelta;
+    private bool countFireDelta;
+
+    public GameObject crosshair;
     public GameObject gun;
     public GameObject bulletPrefab;
     private GameObject bullet;
+    private Vector3 prevRayCastPoint;
 
+    public GameObject particlePrefab;
+    ////////////////////////////////////////////////////////////////////
     ////////////// Variables used for player movement //////////////////
     public Rigidbody rb;
     private int ForwardSpeed = 0;
     private int SidewaysSpeed = 0;
     private int UpwardsSpeed = 0;
+
+    private bool grounded = true;
     ////////////////////////////////////////////////////////////////////
 
     ////////////// Variables used for player and camera rotation ///////
@@ -22,54 +41,123 @@ public class PlayerControlls : MonoBehaviour {
     public float sensitivityY = 15F;
     public float minimumX = -360F;
     public float maximumX = 360F;
-    public float minimumY = -60F;
-    public float maximumY = 60F;
+    public float minimumY = -90F;
+    public float maximumY = 90F;
     float rotationY = 0F;
-    ////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////
     void Update()
     {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        ammunation.text = " " + string.Format("{0:0.}", 1 / deltaTime)/*"Ammo: " + ammo*/;
+        LineOfAimHandler();
         PlayerMovement();
         PlayerAndCameraRotation();
         Shoot();
+        PickUpHandler();
     }
-
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "floor")
+        {
+            grounded = true;
+        }
+    }
     void Start()
     {
+        deltaTime = 0f;
+        fireDelay = 1;
+        countFireDelta = false;
+        fireDelta = 0;
         // Make the rigid body not change rotation
         if (GetComponent<Rigidbody>())
             GetComponent<Rigidbody>().freezeRotation = true;
+        
+    }
+    void PickUpHandler()
+    {
+        if (hitInfo.collider != null && hitInfo.collider.gameObject.tag == "magazine")
+        {
+            if ((hitInfo.collider.gameObject.transform.position - gameObject.transform.position).magnitude <= 0.5f)
+            {
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    ammo += hitInfo.collider.gameObject.GetComponent<AmmoScript>().GetAmountOfBullets();
+                    Destroy(hitInfo.collider.gameObject);
+                }
+            }
+        }
+
+    }
+    void LineOfAimHandler()
+    {
+        // Makes the gun always point at the end of ray (crosshair point)
+        ray = new Ray(camera.transform.position, crosshair.transform.position - camera.transform.position);
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            if (hitInfo.point != prevRayCastPoint)
+            {
+                gun.transform.LookAt(hitInfo.point);
+                prevRayCastPoint = hitInfo.point;
+            }
+            Debug.DrawLine(camera.transform.position, hitInfo.point);
+        }
+        ///////////////////////////////////////////////////////////////////
     }
     void Shoot()
     {
-        RaycastHit hitInfo;
-        Ray ray = new Ray(camera.TransformPoint(camera.position), test.transform.TransformPoint(test.transform.position));
-        if (Physics.Raycast(ray, out hitInfo))
+        // Enable delaycounter (fireDelta++)
+        if (Input.GetMouseButton(0) && !countFireDelta)
         {
-            gun.transform.forward = hitInfo.point - gameObject.gameObject.transform.position;
+            countFireDelta = true;
         }
-
-
-
-        if (Input.GetMouseButtonDown(0))
+        // Shoot a bullet if fireDelta = 0
+        if (fireDelta == 0 && Input.GetMouseButton(0))
         {
-            Vector3 gunPos = gun.transform.position;
+            // Destroying enemies if rayHitInfo holds information about an enemy
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                if (hitInfo.collider.gameObject.tag == "enemy")
+                {
+                    Destroy(hitInfo.collider.gameObject);
+                    // Instantiating particles on target position
+                    Vector3 particlesPos = hitInfo.collider.gameObject.transform.position + new Vector3(0, 0.2f, 0);
+                    Instantiate(particlePrefab, particlesPos, Quaternion.identity);
+                }
+            }
+            ///////////////////////////////////////////////////////////////////
+
+            // Instantiating a bullet for visual effects
+            Vector3 gunPos = gun.transform.position/*new Vector3(camera.position.x, camera.position.y-0.125f, camera.position.z)*/;
             Vector3 gunDirection = gun.transform.forward;
             Quaternion gunRotation = gun.transform.rotation;
             float spawnDistance = 0.055f;
-
             Vector3 spawnPos = gunPos + gunDirection * spawnDistance;
 
             bullet = Instantiate(bulletPrefab, spawnPos, gunRotation) as GameObject;
             bullet.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, bullet.GetComponent<BulletScript>().force));
-
+            bullet.GetComponent<BulletScript>().SetDistance((hitInfo.point - spawnPos).magnitude);
+            ///////////////////////////////////////////////////////////////////
+        }
+        // Count delay for next shot
+        if (countFireDelta)
+        {
+            fireDelta += Time.deltaTime;
+            //Debug.Log(fireDelta + " Fire Delta");
+        }
+        // If (FireDelay) certain time has passed since last bullet was shot, fireDelta becomes 0 and next bullet can be shot
+        if (fireDelta >= gun.GetComponent<GunScript>().FireDelay())
+        {
+            fireDelta = 0;
+            countFireDelta = false;
         }
     }
     void PlayerMovement()
     {
         Vector3 _velocity = rb.velocity;
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && grounded)
         {
             UpwardsSpeed = 10;
+            grounded = false;
         }
         else
         {
@@ -100,7 +188,7 @@ public class PlayerControlls : MonoBehaviour {
             SidewaysSpeed = 0;
         }
         Vector3 direction = new Vector3(SidewaysSpeed, UpwardsSpeed, ForwardSpeed);
-        if (rb.velocity.magnitude <= 3)
+        if (rb.velocity.magnitude <= 1)
         {
             rb.AddRelativeForce(direction * 10);
         }
@@ -122,5 +210,9 @@ public class PlayerControlls : MonoBehaviour {
         {
             transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
         }
+    }
+    public bool Grounded()
+    {
+        return grounded;
     }
 }
