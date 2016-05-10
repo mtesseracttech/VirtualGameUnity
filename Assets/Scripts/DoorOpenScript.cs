e104ab92d5a5dc58c1b4702063538d72f8abdbfb
kaimelis <kaimelisvy24@outlook.com>
2016-05-10 14:11:24 +0200

    door oening and some laser efect

---
+++ Assets/Scripts/DoorOpenScript.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpenScript : MonoBehaviour
+{
+    public bool IsOpen;                 //bool for checking if door is open 
+    public bool IsClosed;               //bool for checking if door is closed
+    public bool InTrigger = false;              //bool for checking if trigger is triggered
+
+    void Start()
+    {
+        IsOpen = false;
+        IsClosed = true;
+    }
+
+    //------------------------------------------------------------------------------------------//
+    //                             When enters the trigger. 
+    //------------------------------------------------------------------------------------------//
+    void OnTriggerEnter(Collider collider)
+    {
+        InTrigger = true;
+    }
+    //------------------------------------------------------------------------------------------//
+    //                             When exits the trigger. 
+    //------------------------------------------------------------------------------------------//
+    void OnTriggerExit(Collider collider)
+    {
+        InTrigger = false;
+    }
+    void Update()
+    {
+        if (InTrigger)  //if it's in collider box
+        {
+            if (IsClosed)   //if door is closed
+            {
+                    if (Input.GetKeyDown(KeyCode.E))    //use E button
+                    {
+                        IsOpen = true;              //can be opened
+                        IsClosed = false;
+                    }
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.E))    //use E button
+                {
+                    IsClosed = true;            //door is closed have a key
+                    IsOpen = false;
+                }
+            }
+        }
+
+        if (IsOpen) //if open is true
+        {
+            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, -90.0f, 0.0f), // representing rotation of open door. 
+                Time.deltaTime * 20);
+            transform.rotation = newRot; //rotating position by using quaternion
+        }
+        else
+        {
+            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, 0.0f, 0.0f), Time.deltaTime * 20);
+            transform.rotation = newRot;
+        }
+    }
+    void OnGUI()
+    {
+        if (InTrigger)
+        {
+            if (IsOpen)
+            {
+                GUI.Box(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 100, 120, 25), "Press E to close");
+            }
+            else
+            {
+                if (IsClosed)
+                {
+                    GUI.Box(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 100, 120, 25), "Press E to open");
+                }
+                
+            }
+        }
+    }
+
+}
--- Assets/Scripts/Player/PlayerControls.cs
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -4,63 +4,63 @@ using UnityEngine.UI;
 
 public class PlayerControls : MonoBehaviour {
    // public Text ammunation;
-    private int ammo;
-    private float deltaTime;
-    public float range = 50;
-    private bool slowMotion;
+    private int _ammo;
+    private float _deltaTime;
+    public float Range = 50;
+    private bool _slowMotion;
 
     ////////////// Variables used for Line of Aim Handling /////////////
-    RaycastHit hitInfo;
-    Ray ray;
+    RaycastHit _hitInfo;
+    Ray _ray;
     ////////////////////////////////////////////////////////////////////
     ////////////// Variables used for raycasting and shooting //////////
-    public float fireDelay;
-    private float fireDelta;
-    private bool countFireDelta;
+    public float FireDelay;
+    private float _fireDelta;
+    private bool _countFireDelta;
 
-    public GameObject crosshair;
-    public GameObject gun;
-    public GameObject bulletPrefab;
+    public GameObject Crosshair;
+    public GameObject Gun;
+    public GameObject BulletPrefab;
     private GameObject bullet;
-    private Vector3 prevRayCastPoint;
+    private Vector3 _prevRayCastPoint;
 
     public ParticleSystem SmokeParticleSystem;
-    public GameObject hitParticles;
-    public GameObject objectHitEffect;
-    private LineRenderer lineRenderer;
-    public Transform gunEnd;
-    private WaitForSeconds shotLength = new WaitForSeconds(.07f);
-    private AudioSource source;
+    public GameObject HitParticles;
+    public GameObject ObjectHitEffect;
+    private LineRenderer _lineRenderer;
+    public Transform GunEnd;
+    private readonly WaitForSeconds _shotLength = new WaitForSeconds(.07f);
+    private AudioSource _source;
 
     ////////////////////////////////////////////////////////////////////
     ////////////// Variables used for player movement //////////////////
-    public Rigidbody rb;
-    private int ForwardSpeed = 0;
-    private int SidewaysSpeed = 0;
-    private int UpwardsSpeed = 0;
+    public Rigidbody Rb;
+    private int _forwardSpeed = 0;
+    private int _sidewaysSpeed = 0;
+    private int _upwardsSpeed = 0;
 
-    private bool grounded = true;
+    private bool _grounded = true;
     ////////////////////////////////////////////////////////////////////
 
     ////////////// Variables used for player and camera rotation ///////
     public Transform camera;
     public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
     public RotationAxes axes = RotationAxes.MouseXAndY;
-    public float sensitivityX = 15F;
-    public float sensitivityY = 15F;
-    public float minimumX = -360F;
-    public float maximumX = 360F;
-    public float minimumY = -90F;
-    public float maximumY = 90F;
-    float rotationY = 0F;
+    public float SensitivityX = 15F;
+    public float SensitivityY = 15F;
+    public float MinimumX = -360F;
+    public float MaximumX = 360F;
+    public float MinimumY = -90F;
+    public float MaximumY = 90F;
+    float _rotationY = 0F;
     ///////////////////////////////////////////////////////////////////
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            slowMotion = !slowMotion;
+            _slowMotion = !_slowMotion;
         }
-        if (slowMotion)
+        if (_slowMotion)
         {
             Time.timeScale = 0.5f;
         }
@@ -68,7 +68,7 @@ public class PlayerControls : MonoBehaviour {
         {
             Time.timeScale = 1f;
         }
-        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        _deltaTime += (Time.deltaTime - _deltaTime) * 0.1f;
        // ammunation.text = " " + string.Format("{0:0.}", 1 / deltaTime)/*"Ammo: " + ammo*/;
         LineOfAimHandler();
         PlayerMovement();
@@ -80,32 +80,32 @@ public class PlayerControls : MonoBehaviour {
     {
         if (collision.gameObject.tag == "floor")
         {
-            grounded = true;
+            _grounded = true;
         }
     }
     void Start()
     {
-        lineRenderer = GetComponent<LineRenderer>();
-        source = GetComponent<AudioSource>();
-        deltaTime = 0f;
-        fireDelay = 1;
-        countFireDelta = false;
-        fireDelta = 0;
+        _lineRenderer = GetComponent<LineRenderer>();
+        _source = GetComponent<AudioSource>();
+        _deltaTime = 0f;
+        FireDelay = 1;
+        _countFireDelta = false;
+        _fireDelta = 0;
         // Make the rigid body not change rotation
         if (GetComponent<Rigidbody>())
             GetComponent<Rigidbody>().freezeRotation = true;
-        slowMotion = false;
+        _slowMotion = false;
     }
     void PickUpHandler()
     {
-        if (hitInfo.collider != null && hitInfo.collider.gameObject.tag == "magazine")
+        if (_hitInfo.collider != null && _hitInfo.collider.gameObject.tag == "magazine")
         {
-            if ((hitInfo.collider.gameObject.transform.position - gameObject.transform.position).magnitude <= 0.5f)
+            if ((_hitInfo.collider.gameObject.transform.position - gameObject.transform.position).magnitude <= 0.5f)
             {
                 if (Input.GetKeyDown(KeyCode.F))
                 {
-                    ammo += hitInfo.collider.gameObject.GetComponent<AmmoScript>().GetAmountOfBullets();
-                    Destroy(hitInfo.collider.gameObject);
+                    _ammo += _hitInfo.collider.gameObject.GetComponent<AmmoScript>().GetAmountOfBullets();
+                    Destroy(_hitInfo.collider.gameObject);
                 }
             }
         }
@@ -115,15 +115,15 @@ public class PlayerControls : MonoBehaviour {
     void LineOfAimHandler()
     {
         // Makes the gun always point at the end of ray (crosshair point)
-        ray = new Ray(camera.transform.position, crosshair.transform.position - camera.transform.position);
-        if (Physics.Raycast(ray, out hitInfo,range))
+        _ray = new Ray(camera.transform.position, Crosshair.transform.position - camera.transform.position);
+        if (Physics.Raycast(_ray, out _hitInfo,Range))
         {           
-            if (hitInfo.point != prevRayCastPoint)
+            if (_hitInfo.point != _prevRayCastPoint)
             {
-                gun.transform.LookAt(hitInfo.point);
-                prevRayCastPoint = hitInfo.point;
+                Gun.transform.LookAt(_hitInfo.point);
+                _prevRayCastPoint = _hitInfo.point;
             }
-            Debug.DrawLine(camera.transform.position, hitInfo.point,Color.blue);
+            Debug.DrawLine(camera.transform.position, _hitInfo.point,Color.blue);
         }
         ///////////////////////////////////////////////////////////////////
     }
@@ -137,38 +137,38 @@ public class PlayerControls : MonoBehaviour {
     }
     private IEnumerator ShotEffect()
     {
-        lineRenderer.enabled = true;
-        source.Play();
-        yield return shotLength;
-        lineRenderer.enabled = false;
+        _lineRenderer.enabled = true;
+        _source.Play();
+        yield return _shotLength;
+        _lineRenderer.enabled = false;
     }
     void Shoot()
     {
         // Enable delaycounter (fireDelta++)
-        if (Input.GetMouseButton(0) && !countFireDelta)
+        if (Input.GetMouseButton(0) && !_countFireDelta)
         {
-            countFireDelta = true;
+            _countFireDelta = true;
             PlayMuzzleFlash();
             
         }
         // Shoot a bullet if fireDelta = 0
-        if (fireDelta == 0 && Input.GetMouseButton(0))
+        if (_fireDelta == 0 && Input.GetMouseButton(0))
         {
             // Destroying enemies if rayHitInfo holds information about an enemy
-            if (Physics.Raycast(ray, out hitInfo))
+            if (Physics.Raycast(_ray, out _hitInfo))
             {
-                if (hitInfo.collider.gameObject.tag == "enemy")
+                if (_hitInfo.collider.gameObject.tag == "enemy")
                 {
-                    Destroy(hitInfo.collider.gameObject);
+                    Destroy(_hitInfo.collider.gameObject);
                     // Instantiating particles on enemy position
-                    Vector3 particlesPos = hitInfo.collider.gameObject.transform.position ;
-                    Instantiate(hitParticles, particlesPos, Quaternion.identity);
+                    Vector3 particlesPos = _hitInfo.collider.gameObject.transform.position ;
+                    Instantiate(HitParticles, particlesPos, Quaternion.identity);
                 }
                 else
                 {
-                     lineRenderer.SetPosition(0, gunEnd.position);
-                     lineRenderer.SetPosition(1, hitInfo.point);
-                     Instantiate(hitParticles, hitInfo.point, Quaternion.identity);
+                     _lineRenderer.SetPosition(0, GunEnd.position);
+                     _lineRenderer.SetPosition(1, _hitInfo.point);
+                     Instantiate(HitParticles, _hitInfo.point, Quaternion.identity);
                 }
                 StartCoroutine(ShotEffect());
             }
@@ -176,70 +176,70 @@ public class PlayerControls : MonoBehaviour {
             ///////////////////////////////////////////////////////////////////
 
             // Instantiating a bullet for visual effects
-            Vector3 gunPos = gun.transform.position/*new Vector3(camera.position.x, camera.position.y-0.125f, camera.position.z)*/;
-            Vector3 gunDirection = gun.transform.forward;
-            Quaternion gunRotation = gun.transform.rotation;
+            Vector3 gunPos = Gun.transform.position/*new Vector3(camera.position.x, camera.position.y-0.125f, camera.position.z)*/;
+            Vector3 gunDirection = Gun.transform.forward;
+            Quaternion gunRotation = Gun.transform.rotation;
             float spawnDistance = 0.055f;
             Vector3 spawnPos = gunPos + gunDirection * spawnDistance;
 
-            bullet = Instantiate(bulletPrefab, spawnPos, gunRotation) as GameObject;
+            bullet = Instantiate(BulletPrefab, spawnPos, gunRotation) as GameObject;
             bullet.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, bullet.GetComponent<BulletScript>().force));
-            bullet.GetComponent<BulletScript>().SetDistance((hitInfo.point - spawnPos).magnitude);
+            bullet.GetComponent<BulletScript>().SetDistance((_hitInfo.point - spawnPos).magnitude);
             ///////////////////////////////////////////////////////////////////
         }
         // Count delay for next shot
-        if (countFireDelta)
+        if (_countFireDelta)
         {
-            fireDelta += Time.deltaTime;
+            _fireDelta += Time.deltaTime;
             //Debug.Log(fireDelta + " Fire Delta");
         }
         // If (FireDelay) certain time has passed since last bullet was shot, fireDelta becomes 0 and next bullet can be shot
-        if (fireDelta >= gun.GetComponent<GunScript>().FireDelay())
+        if (_fireDelta >= Gun.GetComponent<GunScript>().FireDelay())
         {
-            fireDelta = 0;
-            countFireDelta = false;
+            _fireDelta = 0;
+            _countFireDelta = false;
         }
     }
     void PlayerMovement()
     {
-        Vector3 _velocity = rb.velocity;
-        if (Input.GetKey(KeyCode.Space) && grounded)
+        Vector3 _velocity = Rb.velocity;
+        if (Input.GetKey(KeyCode.Space) && _grounded)
         {
-            UpwardsSpeed = 10;
-            grounded = false;
+            _upwardsSpeed = 10;
+            _grounded = false;
         }
         else
         {
-            UpwardsSpeed = 0;
+            _upwardsSpeed = 0;
         }
         if (Input.GetKey(KeyCode.W))
         {
-            ForwardSpeed = 1;
+            _forwardSpeed = 1;
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            ForwardSpeed = -1;
+            _forwardSpeed = -1;
         }
         else
         {
-            ForwardSpeed = 0;
+            _forwardSpeed = 0;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            SidewaysSpeed = -1;
+            _sidewaysSpeed = -1;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            SidewaysSpeed = 1;
+            _sidewaysSpeed = 1;
         }
         else
         {
-            SidewaysSpeed = 0;
+            _sidewaysSpeed = 0;
         }
-        Vector3 direction = new Vector3(SidewaysSpeed, UpwardsSpeed, ForwardSpeed);
-        if (rb.velocity.magnitude <= 1)
+        Vector3 direction = new Vector3(_sidewaysSpeed, _upwardsSpeed, _forwardSpeed);
+        if (Rb.velocity.magnitude <= 1)
         {
-            rb.AddRelativeForce(direction * 10);
+            Rb.AddRelativeForce(direction * 10);
         }
         //Debug.Log("Walk: " + _velocity.magnitude);
     }
@@ -247,22 +247,22 @@ public class PlayerControls : MonoBehaviour {
     {
         if (axes == RotationAxes.MouseXAndY)
         {
-            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * SensitivityX;
 
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+            _rotationY += Input.GetAxis("Mouse Y") * SensitivityY;
+            _rotationY = Mathf.Clamp(_rotationY, MinimumY, MaximumY);
 
             transform.localEulerAngles = new Vector3(0, rotationX, 0);
-            camera.localEulerAngles = new Vector3(-rotationY, 0, 0);
+            camera.localEulerAngles = new Vector3(-_rotationY, 0, 0);
         }
         else if (axes == RotationAxes.MouseX)
         {
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+            transform.Rotate(0, Input.GetAxis("Mouse X") * SensitivityX, 0);
         }
     }
     public bool Grounded()
     {
-        return grounded;
+        return _grounded;
     }
 
     
