ed65d0a7c4ad34c018fdf4e66d9f3f59f2c567c1
kaimelis <kaimelisvy24@outlook.com>
2016-05-10 17:23:30 +0200

    menu added with loading screen

---
+++ Assets/Scripts/Menu/LoadingScene.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScene : MonoBehaviour
+{
+    private bool _loadScene;
+    [SerializeField]
+    private int _scene;
+    [SerializeField]
+    private Text _loadingText;
+
+    void Update()
+    {
+        if (!_loadScene)
+        {
+            _loadScene = true;
+            _loadingText.text = "Loading...";
+            StartCoroutine(LoadNewScene());
+        }
+
+        if (_loadScene)
+        {
+            _loadingText.color = new Color(_loadingText.color.r, _loadingText.color.g, _loadingText.color.b, Mathf.PingPong(Time.time, 1));
+        }
+    }
+    IEnumerator LoadNewScene()
+    {
+        yield return new WaitForSeconds(5);
+        AsyncOperation async = SceneManager.LoadSceneAsync(_scene);
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+
+    }
+
+}
