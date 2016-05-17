fb8f3f00f91199dca56474c4d801da53210bda48
MT <mtesseracttech@gmail.com>
2016-05-17 11:11:01 +0200

    Some changes to some particles

--- Assets/Scripts/Enemy/enemyScript.cs
+++ Assets/Scripts/Enemy/enemyScript.cs
@@ -10,6 +10,8 @@ public class EnemyScript : MonoBehaviour
     private Transform _turret;
     private EnemyAgent _agent;
     private Renderer _laserRenderer;
+    private readonly Vector3 _normalscale = new Vector3(1,1,1);
+    private readonly Vector3 _invisiblescale = new Vector3(0,0,0);
 
 
     void Awake()
@@ -29,12 +31,14 @@ public class EnemyScript : MonoBehaviour
         _turretPoint = _turret.transform.position;
         if (_agent.GetState() is AttackState)
         {
+            //LaserLine.transform.localScale = _normalscale;
             Debug.Log("SHOULD BE DRAWING A LASER");
             LaserLine.SetPosition(0, _turretPoint);
             LaserLine.SetPosition(1, Player.transform.position);
         }
         else
         {
+            //LaserLine.transform.localScale = _invisiblescale;
             //MAKE LINE INVISIBLE!!!
         }
     }
